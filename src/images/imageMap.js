1b7b20f7973f10e8932e5bee0c4913dcb93d8206
Kshivaji333 <ks3653817@gmail.com>
2025-09-10 03:30:00 +0530

    add images to the ui

--- src/images/imageMap.js
+++ src/images/imageMap.js
@@ -1,4 +1,6 @@
 export const imageMap = {
   tandoori_chicken: require('./Tandoori_Chicken.png'),
+  dal_makhni: require('./dal_makhni.png'),
+  cabbage: require('./cabbage.png'),
   default: require('./placeholder.png'),
 };
